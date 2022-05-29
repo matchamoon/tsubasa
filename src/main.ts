c30470baf37096197bd4313db5d27846248f7489
Nightscape Coffee <23473065+coffeebank@users.noreply.github.com>
2022-05-29 02:28:23 -0700

    refactor: add dependencies
    Add Vuex, Vue Router, new Tailwind colors

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,27 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import { routes } from "./routes.js"
+import { createRouter, createWebHistory } from "vue-router"
+import { createStore } from "vuex"
 import "./assets/tailwind.css";
 
-createApp(App).mount("#app");
+const store = createStore({
+  state () {
+    return {
+      copyYear: '',
+    }
+  },
+  mutations: {
+  }
+})
+
+const app = createApp(App)
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+app.use(router)
+app.use(store)
+app.mount('#app');
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        '161616': '#161616',
+        '1a1a1a': '#1a1a1a',
+        '202020': '#202020',
+        '222222': '#222222',
+        '282828': '#282828',
+        '2f2f2f': '#2f2f2f',
+        '323232': '#323232',
+        'cccccc': "#cccccc",
+        'eeeeee': "#eeeeee",
+      },
+    },
   },
   plugins: [],
 }
