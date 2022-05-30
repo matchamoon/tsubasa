2e0b0a38990fdb372d0fbcf3258836fb70d325b9
milkyskies <me@yozora.dev>
2022-05-30 18:18:22 +0900

    refactor: Format store.index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,16 @@
-import type { InjectionKey } from "vue"
-import { createStore, useStore as baseUseStore, Store } from "vuex"
+import type { InjectionKey } from "vue";
+import { createStore, useStore as baseUseStore, Store } from "vuex";
 
 export interface State {
-  consoleErr: string,
-  consoleMsg: string,
-  ffFilename: string,
-  ffFileOk: boolean,
-  ffFilesize: number,
-  ffProgress: number
+  consoleErr: string;
+  consoleMsg: string;
+  ffFilename: string;
+  ffFileOk: boolean;
+  ffFilesize: number;
+  ffProgress: number;
 }
 
-export const key: InjectionKey<Store<State>> = Symbol()
+export const key: InjectionKey<Store<State>> = Symbol();
 
 export const store = createStore<State>({
   state: {
@@ -19,33 +19,33 @@ export const store = createStore<State>({
     ffFilename: "",
     ffFileOk: false,
     ffFilesize: 0,
-    ffProgress: 0
+    ffProgress: 0,
   },
   mutations: {
-    consoleErr (state, data) {
-      state.consoleErr = data
+    consoleErr(state, data) {
+      state.consoleErr = data;
     },
-    consoleMsg (state, data) {
-      state.consoleMsg = data
+    consoleMsg(state, data) {
+      state.consoleMsg = data;
     },
-    ffFilename (state, data) {
-      state.ffFilename = data
+    ffFilename(state, data) {
+      state.ffFilename = data;
     },
-    ffFileOk (state, data) {
-      state.ffFileOk = data
+    ffFileOk(state, data) {
+      state.ffFileOk = data;
     },
-    ffFilesize (state, data) {
-      state.ffFilesize = data
+    ffFilesize(state, data) {
+      state.ffFilesize = data;
     },
-    ffProgress (state, data) {
+    ffProgress(state, data) {
       if (data > 0 && data < 101) {
-        state.ffProgress = data
+        state.ffProgress = data;
       }
     },
-  }
-})
+  },
+});
 
 // define your own `useStore` composition function
-export function useStore () {
-  return baseUseStore(key)
+export function useStore() {
+  return baseUseStore(key);
 }
