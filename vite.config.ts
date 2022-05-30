d6963b25f71db9176c634da31d5658c6d0c8c62a
Nightscape Coffee <23473065+coffeebank@users.noreply.github.com>
2022-05-29 23:11:50 -0700

    feat: Add PWA
    Add manifest for local caching
    #7: Add meta tags

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,22 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { viteStaticCopy } from "vite-plugin-static-copy";
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VitePWA({
+      registerType: 'autoUpdate', 
+      manifest: {
+        name: "Tsubasa 🦋",
+        short_name: "Tsubasa",
+        description: "Video compression made easy. Maintain the best quality while resizing your mp4 files to any size, for any purpose. Fast and private transcoding locally on any device.",
+        background_color: "#6366f1",
+        theme_color: "#6366f1",
+      },
+    }),
     viteStaticCopy({
       targets: [
         {
