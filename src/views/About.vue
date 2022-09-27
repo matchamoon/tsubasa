de7f339837f180dd2d4c345dbb96c180f796461c
Nightscape Coffee <23473065+coffeebank@users.noreply.github.com>
2022-09-27 03:00:00 -0700

    feat: Add Privacy Policy

--- src/views/About.vue
+++ src/views/About.vue
@@ -18,6 +18,8 @@
       <p class="py-2">With ffmpeg-wasm, transcoding can happen locally on devices, which means no time spent upload/downloading and processing as fast as your device allows. We were also able to experiment with preserving audio quality, which we found minimally impacted the output file size while improving the overall "feel" even for lower resolution videos. By allowing videos to scale the resolution down, more file space can be allocated to preserving the bitrate and the quality of each frame.</p>
       <p class="py-2">Tsubasa is a work in progress, and welcomes feature suggestions, bug reports, and pull requests.</p>
       <p class="py-2">Source code: <a href="https://github.com/matchamoon/tsubasa" target="_blank" rel="noopener" class="text-indigo-300 hover:text-indigo-200 transition duration-150">https://github.com/matchamoon/tsubasa</a></p>
+      <h1 class="text-2xl font-bold leading-tight pt-6 pb-2 text-gray-100">Privacy Policy</h1>
+      <p class="py-2">We do not respond to DNT headers. We do not store any other data.</p>
       <h1 class="text-2xl font-bold leading-tight pt-6 pb-2 text-gray-100">Licenses</h1>
       <p class="py-2">License: <a href="https://github.com/matchamoon/tsubasa/blob/master/LICENSE" target="_blank" rel="noopener" class="text-indigo-300 hover:text-indigo-200 transition duration-150 break-words">https://github.com/matchamoon/tsubasa/blob/master/LICENSE</a></p>
       <p class="py-2">Tsubasa was built using:</p>
